7b32551ea4b114d2e2e6de5a26346f1ddec2ebc5
Your Name <hussainafzaal071@gmail.com>
2022-01-05 01:17:23 +0500

    first commitment done

---
+++ AirlineManagementSystemOOAD/AirlineManagementSystemOOAD/Controllers/CustomerController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using AirlineManagementSystemOOAD.Models;
+
+namespace AirlineManagementSystemOOAD.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+        private airline_managementEntities db = new airline_managementEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(string name, string pass)
+        {
+            var log = db.Logins.Where(x => x.Name == name && x.Password == pass).FirstOrDefault();
+            if (log != null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "Please Enter Correct User Name Or Password!";
+            }
+            return View();
+        }
+
+        public ActionResult Booking(int id = 0)
+        {
+            TicketBook tb = new TicketBook();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Booking(TicketBook t)
+        {
+            using (airline_managementEntities ticket = new airline_managementEntities())
+            {
+                if (t.FlyingFrom == "-Select-" || t.FlyingTo == "-Select-")
+                {
+                    ViewBag.message = "Please Enter City";
+                }
+                else if (t.FlyingFrom == t.FlyingTo)
+                {
+                    ViewBag.message = "Error! Locations Are Same: ";
+                }
+                else if (t.Gender == "-Select-")
+                {
+                    ViewBag.message = "Please Enter Gender ";
+                }
+                else
+                {
+                    if (t.FClass == "Business class")
+                    {
+
+                        t.TotalAmount = t.Adults * 12000;
+                        t.n = Convert.ToInt32(t.TotalAmount + (t.Child * 8000));
+                        t.TotalAmount = t.n;
+                        ticket.TicketBooks.Add(t);
+                        ticket.SaveChanges();
+                        ModelState.Clear();
+                        ViewBag.Message = "Success";
+                        return RedirectToAction("Details");
+
+                    }
+                    else if (t.FClass == "Economy class")
+                    {
+                        t.TotalAmount = t.Adults * 8000;
+                        t.n = Convert.ToInt32(t.TotalAmount + (t.Child * 4000));
+                        t.TotalAmount = t.n;
+                        ticket.TicketBooks.Add(t);
+                        ticket.SaveChanges();
+                        ModelState.Clear();
+                        ViewBag.Message = "Success";
+                        return RedirectToAction("Details");
+                    }
+                    else if (t.FClass == "First class")
+                    {
+                        t.TotalAmount = t.Adults * 10000;
+                        t.n = Convert.ToInt32(t.TotalAmount + (t.Child * 6000));
+                        t.TotalAmount = t.n;
+                        ticket.TicketBooks.Add(t);
+                        ticket.SaveChanges();
+                        ModelState.Clear();
+                        ViewBag.Message = "Success";
+                        return RedirectToAction("Details");
+                    }
+                }
+            }
+            return View("Booking", new TicketBook());
+        }
+        public ActionResult Details()
+        {
+            var fetch = db.TicketBooks.ToList();
+            return View(fetch);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ AirlineManagementSystemOOAD/AirlineManagementSystemOOAD/Models/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineManagementSystemOOAD.Models
+{
+    public class Book
+    {
+        [Required(ErrorMessage = "Enter First Name! ")]
+        [Display(Name = "Enter First Name! ")]
+        [StringLength(maximumLength: 7, MinimumLength = 3, ErrorMessage = "First Name Must be Max 7 & Min 3 Letters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Enter Last Name! ")]
+        [Display(Name = "Enter Last Name! ")]
+        [StringLength(maximumLength: 7, MinimumLength = 3, ErrorMessage = "Last Name Must be Max 7 & Min 3 Letters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Enter Dob Name! ")]
+        [Display(Name = "Enter Last Name! ")]
+
+        public string Dob { get; set; }
+
+        [Required(ErrorMessage = "Enter Gender! ")]
+        [Display(Name = "Enter Gender! ")]
+        public string gender { get; set; }
+
+        [Required(ErrorMessage = "Enter CNIC! ")]
+        [Display(Name = "Enter CNIC! ")]
+        public long cnic { get; set; }
+        [Required(ErrorMessage = "Enter Phone Number! ")]
+        [Display(Name = "Enter Phone Number! ")]
+        [StringLength(maximumLength: 11, MinimumLength = 11, ErrorMessage = "Last Name Must be Max 7 & Min 3 Letters")]
+        public long phone { get; set; }
+        [Required(ErrorMessage = "Enter Arrival! ")]
+        [Display(Name = "Enter Arrival! ")]
+        public string arrival { get; set; }
+        [Required(ErrorMessage = "Enter Departure! ")]
+        [Display(Name = "Enter Departure! ")]
+        public string dept { get; set; }
+        [Required(ErrorMessage = "Enter Departure Date! ")]
+        [Display(Name = "Enter Departure Date! ")]
+        public string dept_date { get; set; }
+        [Required(ErrorMessage = "Enter Return Date! ")]
+        [Display(Name = "Enter Return Date! ")]
+        public string return_date { get; set; }
+        [Required(ErrorMessage = "Enter Adults! ")]
+        [Display(Name = "Enter Adults! ")]
+        public int adult { get; set; }
+        public int child { get; set; }
+        [Required(ErrorMessage = "Enter Travel Class! ")]
+        [Display(Name = "Enter Travel Class! ")]
+        public string travelclass { get; set; }
+        public int seatsavailable { get; set; }
+        public int totalamount { get; set; }
+    }
+}
